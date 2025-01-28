f6cd098f4e4f2dd1ffe3ba8712ed54f374a164e8
David Leyland <david@plain.com>
2025-01-28 16:54:28 +0000

    lint

--- chat.js
+++ chat.js
@@ -1,10 +1,10 @@
-script = document.createElement('script');
+script = document.createElement("script");
 script.async = false;
-script.onload = function () {
-  Plain.init({
-    appId: 'liveChatApp_01J9BNACEDQHJ5KNCBB3JSDHWR',
-    requireAuthentication: true
-  });
+script.onload = () => {
+	Plain.init({
+		appId: "liveChatApp_01J9BNACEDQHJ5KNCBB3JSDHWR",
+		requireAuthentication: true,
+	});
 };
-script.src = 'https://chat.cdn-plain.com/index.js';
-document.getElementsByTagName('head')[0].appendChild(script);
+script.src = "https://chat.cdn-plain.com/index.js";
+document.getElementsByTagName("head")[0].appendChild(script);
